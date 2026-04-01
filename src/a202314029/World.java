e2e855b574491f29cd9a3cda69b9db3e445df46d
kimsieun <mkdirtory@gmail.com>
2026-04-01 10:46:36 +0900

    five

--- src/a202314029/World.java
+++ src/a202314029/World.java
@@ -1,7 +1,10 @@
 package a202314029;
 
-public class World {
-public static void run() {
-System.out.println("202314029");
-}
+  public class World {
+  public static void run() {
+    System.out.println("202314029");
+  }
+  public static void run2() {
+    System.out.println("hello world");
+  }
 }
